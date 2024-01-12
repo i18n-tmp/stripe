1ffff539adc60c0184fafc73390a535c37439ae5
i18n <i18n.site@gmail.com>
2024-01-12 17:01:06 +0800

    ♨

--- web/sh/dev.sh
+++ web/sh/dev.sh
@@ -9,7 +9,9 @@ VITE_PORT=${VITE_PORT:-5173}
 
 kill -9 $(lsof -i:$VITE_PORT -t) 2>/dev/null | true
 
-bash -c "sleep 1 && open https://127.0.0.1:$VITE_PORT" &
+if ! command -v open &>/dev/null; then
+  bash -c "sleep 1 && open https://127.0.0.1:$VITE_PORT" &
+fi
 
 bun x concurrently --kill-others \
   -r "bun x vite"
