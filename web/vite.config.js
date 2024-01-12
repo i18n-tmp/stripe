8a3133fc3e6902006f73074b499875d8e4cf9c97
i18n <i18n.site@gmail.com>
2024-01-12 17:20:14 +0800

    .

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,5 +1,6 @@
 import uridir from "@3-/uridir";
 import viteConf from "@3-/vite-conf";
+const ROOT = uridir(import.meta);
 const conf = await viteConf(ROOT);
 conf.server.https = false;
 export default conf;
